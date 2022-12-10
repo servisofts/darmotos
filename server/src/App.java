3beb3770edf8809d7d28cd22a53e1c705ac2534c
Roy Ruddy Paz Demiquel <ruddypazd@gmail.com>
2022-12-10 13:30:04 -0400

    sdf

--- server/src/App.java
+++ server/src/App.java
@@ -1,17 +1,13 @@
 import Servisofts.Servisofts;
 
-public class App {
+public class App {  
     public static void main(String[] args) {
         try {
-
-            
-
             Servisofts.DEBUG = false;
             Servisofts.ManejadorCliente = ManejadorCliente::onMessage;
             Servisofts.Manejador = Manejador::onMessage;
             Servisofts.initialize();
         } catch (Exception e) {
-            
             e.printStackTrace();
         }
     }
